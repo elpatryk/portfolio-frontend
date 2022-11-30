1592c00826070ddc07b68d476e06cc288da7b571
elpatryk <patrykzmurko@yahoo.com>
2022-11-30 16:47:08 +0100

    first round of events done

--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -1,15 +1,33 @@
-import React, { useEffect } from "react";
-import { Button } from "../styled";
+import React, { useEffect, useState } from "react";
+import { Button, Input } from "../styled";
 import { useDispatch, useSelector } from "react-redux";
 import { selectEvent } from "../store/events/selectors";
-import { getEvents } from "../store/events/thunks";
+import {
+  getEvents,
+  postEvent,
+  postEventParticipations,
+} from "../store/events/thunks";
 import { Link } from "react-router-dom";
+
 export default function EventPage() {
   const dispatch = useDispatch();
   const event = useSelector(selectEvent);
+
+  const [description, setDescription] = useState();
+  const [pitchId, setPitchId] = useState();
+  const [capacity, setCapacity] = useState();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    dispatch(postEvent(description, pitchId, capacity));
+  };
   useEffect(() => {
     dispatch(getEvents());
   }, []);
+
+  const onClickJoin = (eventId) => {
+    dispatch(postEventParticipations(eventId));
+  };
 
   return (
     <div>
@@ -18,11 +36,39 @@ export default function EventPage() {
         ? "loading"
         : event.map((e) => {
             return (
-              <Link to={`/events/${e.id}`}>
-                <Button key={e.id}>{e.description} </Button>{" "}
-              </Link>
+              <div className="events">
+                {" "}
+                <Button onClick={() => onClickJoin(e.id)}>Join to Event</Button>
+                <Link to={`/events/${e.id}`}>
+                  <div className="events" key={e.id}>
+                    {e.description}
+                  </div>{" "}
+                </Link>
+              </div>
             );
           })}
+      <Button>New event?</Button>
+      <form onSubmit={submitForm}>
+        <Input
+          placeholder="title for event"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <Input
+          type="number"
+          placeholder="on which pitch you wanna play?"
+          value={pitchId}
+          onChange={(e) => setPitchId(e.target.value)}
+        />{" "}
+        <Input
+          type="number"
+          placeholder="how many teams you want in this event?"
+          value={capacity}
+          onChange={(e) => setCapacity(e.target.value)}
+        />
+        <br />
+        <Button type="submit">Create new Event</Button>
+      </form>
     </div>
   );
 }
--- src/store/events/thunks.js
+++ src/store/events/thunks.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { appDoneLoading, appLoading, setMessage } from "../appState/slice";
+import { showMessageWithTimeout } from "../appState/thunks";
 import { allEvents, specificEvent } from "./slice";
 
 const apiUrl = "http://localhost:4000";
@@ -19,6 +21,73 @@ export const eventById = (id) => {
       const response = await axios.get(`${apiUrl}/events/${id}`);
       console.log("Thunk eventbyId", response);
       dispatch(specificEvent(response.data));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
+
+export const postEvent = (description, pitchId, capacity) => {
+  return async (dispatch, getState) => {
+    const { token } = getState().user;
+
+    try {
+      const response = await axios.post(
+        `${apiUrl}/events/new`,
+        { description, pitchId, capacity },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("thunk postEvent ", response);
+      dispatch(showMessageWithTimeout("succes", false, "New event!", 1500));
+    } catch (error) {
+      if (error.response) {
+        dispatch(
+          setMessage({
+            variant: "danger",
+            dismissable: true,
+            text: error.response.data.message,
+          })
+        );
+      } else {
+        console.log(error.message);
+        dispatch(
+          setMessage({
+            variant: "danger",
+            dismissable: true,
+            text: error.response.data.message,
+          })
+        );
+      }
+    }
+  };
+};
+
+export const postEventParticipations = (eventId) => {
+  return async (dispatch, getState) => {
+    const { token } = getState().user;
+    try {
+      const response = await axios.post(
+        `${apiUrl}/events/join`,
+        { eventId },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("eventParticipation thunk: ", response);
+      dispatch(
+        showMessageWithTimeout(
+          "succes",
+          false,
+          "Good luck in your new event!",
+          1500
+        )
+      );
     } catch (e) {
       console.log(e);
     }
